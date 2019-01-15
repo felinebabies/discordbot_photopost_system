5a8b07803661fc374a980986023bd6b1c7a19cce
Syuji <commentonly.is@gmail.com>
2019-01-16 01:03:10 +0900

    1イベントあたりに送信する写真数を5に限定した。

--- catphoto_sendtodiscord.rb
+++ catphoto_sendtodiscord.rb
@@ -8,6 +8,8 @@ TARGETCHANNEL = "cat_photo_system"
 
 CAT_PHOTO_DIR = ENV["PHOTO_DIR_PASS"]
 
+MAX_PHOTO_PER_EVENT = 5
+
 def post_photo(botobj, targetch, imagepath, caption)
     botobj.send_file(targetch, File.open(imagepath, 'r'), caption: caption)
 end
@@ -28,7 +30,15 @@ ch_id = bot.find_channel(TARGETCHANNEL).first.id
 if ch_id != nil then
     photos = Dir.glob("#{CAT_PHOTO_DIR}/*.jpg")
 
-    photos.each do |additem|
+    photos.reverse!
+
+    if photos.size > MAX_PHOTO_PER_EVENT then
+        targets = MAX_PHOTO_PER_EVENT.times.map{|i| photos[photos.size*i/MAX_PHOTO_PER_EVENT ... photos.size*(i+1)/MAX_PHOTO_PER_EVENT]}.map{|i| i.first }
+    else
+        targets = photos
+    end
+
+    targets.each do |additem|
         post_photo(bot, ch_id, additem, File.basename(additem))
 
         sleep 3
